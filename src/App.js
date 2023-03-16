b14132f9edd3675b649ee9e212a19116193bfc6c
lolotrgeek <jon@simplyenable.com>
2023-03-16 15:18:42 -0500

    feat: basic action

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 
 function App() {
   const [data, setData] = useState([{}]);
+  const [state, setState] = useState([0]);
 
   const fetchData = async params => {
     let query = `http://localhost:8002/impact?`
@@ -17,25 +18,37 @@ function App() {
       })
     }
     else {
-      query += `${Object.keys(params)[0]}=${Object.values(params)[0]}`
+      let keys = Object.keys(params)
+      let values = Object.values(params)
+      keys.map((key, index) => {
+        if (index > 0) query += '&'
+        query += `${key}=${values[index]}`
+        return key
+      })
     }
+    console.log(query)
     const response = await fetch(query);
     const jsonData = await response.json();
     console.log(jsonData);
-    setData(!Array.isArray(jsonData) ? [jsonData] : jsonData);
+    setData(jsonData);
   };
   useEffect(() => {
     fetchData({ initial: true });
   }, []);
 
-  const handleFormSubmit = params => {
+  const handleAction = params => {
+    setState([...state, params.after])
     console.log(params)
     fetchData(params);
   };
 
   return (
     <div>
-      {JSON.stringify(data)}
+      Impact: {data.impact}
+      <br />
+      Current State: {state[state.length-1]}
+      <br />
+      <button type="text" name="action"  onClick={() => handleAction({action: 1, before: state[state.length-1], after: state[state.length-1]+1})}>Action!</button>
     </div>
   )
 }
