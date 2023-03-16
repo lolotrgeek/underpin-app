47a814f330d2c3a348548d3ca7393f127fe35c4a
lolotrgeek <jon@simplyenable.com>
2023-03-16 14:41:39 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
 
 function App() {
+  const [data, setData] = useState([{}]);
+
+  const fetchData = async params => {
+    let query = `http://localhost:8002/impact?`
+    if (Array.isArray(params)) {
+      params.map((param, index) => {
+        if (param.value) {
+          if (index > 0) query += '&'
+          query += `${param.key}=${param.value}`
+          console.log(query)
+        }
+        return param
+      })
+    }
+    else {
+      query += `${Object.keys(params)[0]}=${Object.values(params)[0]}`
+    }
+    const response = await fetch(query);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    setData(!Array.isArray(jsonData) ? [jsonData] : jsonData);
+  };
+  useEffect(() => {
+    fetchData({ initial: true });
+  }, []);
+
+  const handleFormSubmit = params => {
+    console.log(params)
+    fetchData(params);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(data)}
     </div>
-  );
+  )
 }
 
 export default App;
