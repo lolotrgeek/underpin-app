1f383fab9f75d50a9a36d662604f75f773cc2883
lolotrgeek <jon@simplyenable.com>
2023-03-25 17:00:07 -0500

    feat: initial private network

--- src/App.js
+++ src/App.js
@@ -3,80 +3,171 @@ import './styles/App.css';
 import * as IPFS from 'ipfs-core'
 import OrbitDB from 'orbit-db'
 import { calc_impact } from './app/impact';
+import Protector from 'libp2p-pnet'
 
-const createBlock = async (key, value, db) => await db.add({ key, value })
-const readBlock = async (hash, db) => await db.get(hash)
-
+const swarmKeyBuffer = Buffer("2F6B65792F737761726D2F70736B2F312E302E302F0A2F6261736531362F0A356238393035363166396262333532323531313233323765613333643661393634666331376636366434396432366562613262333162626339653439636461630A", "hex")
 
 function App() {
-  const [block, setBlock] = useState([{}])
+  const [block, setBlock] = useState('')
   const [impact, setImpact] = useState(0)
   const [state, setState] = useState(0)
   const [actor, setActor] = useState(crypto.randomUUID())
   const [ipfs, setIpfs] = useState(null)
   const [db, setDb] = useState(null)
 
-  // const createBlock = (key, value) => new TextEncoder().encode(JSON.stringify({key, value}))
-  // const readBlock = block => new TextDecoder().decode(JSON.parse(block))
-
-
+  const writeBlock = data => new TextEncoder().encode(JSON.stringify(data))
+  const readBlock = block => new TextDecoder().decode(JSON.parse(block))
 
   useEffect(() => {
     const init = async () => {
-      let name = { repo: 'underpin' }
-      let new_ipfs = await IPFS.create(name)
-      setIpfs(new_ipfs)
-      const orbitdb = await OrbitDB.createInstance(ipfs)
-      const new_db = await orbitdb.eventlog(name.repo)
-      setDb(new_db)
-      await db.load()
-      // Listen for updates from peers
-      db.events.on("replicated", address => {
-        console.log(db.iterator({ limit: -1 }).collect())
-      })
-      let hash = await createBlock(actor + '_impact', 0, db)
-      setBlock(hash)
-      setImpact(0)
-    }
-    const cleanup = async () => {
-      await db.stop()
+      try {
+        let new_ipfs = await IPFS.create({
+          repo: 'underpin' + Math.random(),
+          EXPERIMENTAL: { pubsub: true },
+          preload: { "enabled": false },
+          config: {
+            Pubsub: {
+              enabled: true,
+              PubSubRouter: "gossipsub",
+              Router: "gossipsub",
+            },
+            Addresses: {
+              Swarm: [
+                // "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
+                // "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
+              ],
+              Bootstrap: [
+                "/ip4/192.168.50.57/tcp/4001/p2p/12D3KooWPs316f6ZuVhUXa3bZaA6W37YWHVud3on6wyprU65fjhi",
+                "/ip4/192.168.50.57/tcp/4001/ws/p2p/12D3KooWPs316f6ZuVhUXa3bZaA6W37YWHVud3on6wyprU65fjhi",
+              ],
+
+              API: "/ip4/192.168.50.57/tcp/5001",
+              Gateway: "/ip4/192.168.50.57/tcp/8080",
+
+            },
+            API: {
+              "HTTPHeaders": {
+                "Access-Control-Allow-Origin": [
+                  "http://127.0.0.1:3000",
+                  "http://192.168.50.57:4001",
+                ]
+              }
+            },
+            Bootstrap: [
+              "/ip4/192.168.50.57/tcp/4001/ws/p2p/12D3KooWPs316f6ZuVhUXa3bZaA6W37YWHVud3on6wyprU65fjhi"
+            ],
+          },
+          libp2p: {
+            modules: {
+              connProtector: new Protector(swarmKeyBuffer),
+            },
+            config: {
+              bootstrap: {
+                interval: 10000,
+                enabled: true,
+                list: [
+                  '/ip4/192.168.50.57/tcp/4001/p2p/12D3KooWPs316f6ZuVhUXa3bZaA6W37YWHVud3on6wyprU65fjhi',
+                  "/ip4/192.168.50.57/tcp/4001/ws/p2p/12D3KooWPs316f6ZuVhUXa3bZaA6W37YWHVud3on6wyprU65fjhi",
+                ]
+              }
+            }
+          }
+
+        })
+        setIpfs(new_ipfs)
+        setImpact(0)
+      } catch (e) {
+        console.log(e)
+      }
     }
 
     init()
     return () => {
       console.log('unmounting')
-      cleanup()
     }
   }, [])
 
-  // useEffect(() => {
-  //   const update = async () =>{let last_block = await readBlock(block, ipfs);console.log(last_block)}
-  //   update()
-  // }, [state])
+  useEffect(() => {
+    const loadOrbit = async () => {
+      try {
+        if (!ipfs) return
+        const orbitdb = await OrbitDB.createInstance(ipfs)
+        const new_db = await orbitdb.log("underpin")
+        setDb(new_db)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    loadOrbit()
+  }, [ipfs])
+
+  useEffect(() => {
+    const loadDB = async () => {
+      try {
+        if (!db) return
+        await db.load()
+        // Listen for updates from peers
+        db.events.on("replicated", address => {
+          console.log(db.iterator({ limit: 1 }).collect())
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    loadDB()
+  }, [db])
+
+
 
   const handleAction = async params => {
-    setState(params.state)
-    console.log(params)
-    let impact_prediction = calc_impact({ before: state, after: params.state }, params.action)
-    let hash = await createBlock(actor + '_impact', impact_prediction, db)
-    setImpact(impact_prediction.impact)
-    setBlock(hash)
+    try {
+      setState(params.state)
+      console.log(params)
+      let impact_prediction = calc_impact({ before: state, after: params.state }, params.action)
+      // let hash = await addBlock({ actor, impact: impact_prediction.impact, action: params.action, state: params.state }, db)
+      let hash = await db.add({ actor, impact: impact_prediction.impact, action: params.action, state: params.state })
+      console.log(hash)
+      setImpact(impact_prediction.impact)
+      // setBlock(hash)
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 
+  const handleUpdate = async () => {
+    try {
+      // let last_block = await db.get(block);
+      let block = await db.iterator({ limit: 1, reverse: true }).collect()[0]
+      console.log(block);
+      setImpact(block.payload.value.impact)
+      setState(block.payload.value.state)
+      setBlock(block.hash)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div>
-      Actor: {actor}
-      <br />
-      Impact: {impact}
-      <br />
-      Current State: {state}
-      <br />
-      Last Block: {block}
-      <br />
-      <button type="text" name="action" onClick={() => handleAction({ action: 1, state: state + 1 })}>Action!</button>
-      <br />
-      <button type="text" name="update" onClick={async () => { let last_block = await readBlock(block, db); console.log(last_block); setImpact(last_block.value.value.impact) }}>Update From Chain !</button>
+      {db ? (
+        <div>
+          Actor: {actor}
+          <br />
+          Impact: {impact}
+          <br />
+          Current State: {state}
+          <br />
+          Last Block: {block}
+          <br />
+          <button type="text" name="action" onClick={() => handleAction({ action: 1, state: state + 1 })}>Action!</button>
+          <br />
+          <button type="text" name="update" onClick={handleUpdate}>Update From Chain !</button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
